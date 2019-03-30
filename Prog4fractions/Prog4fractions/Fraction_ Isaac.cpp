731b0ad8a67a93ae2a1bb141bc9d3bf8b7bcb6de
Joachim Isaac <joachimvivian3@gmail.com>
2019-03-29 23:27:21 -0500

    Add files via upload

--- Prog4fractions/Prog4fractions/Fraction_ Isaac.cpp
+++ Prog4fractions/Prog4fractions/Fraction_ Isaac.cpp
@@ -13,14 +13,15 @@ using namespace std;
 Fraction :: Fraction(){
     num = 4;
     den = 5;
+    
 }
 
-Fraction :: Fraction(int n , int d){
+Fraction :: Fraction( int n ,  int d){
     
     num = n;
     den = d;
-    
-    
+    reduce_fraction();
+   
 }
 
 
@@ -43,10 +44,10 @@ void Fraction :: reduce_fraction(){
         }
     }
     
-    
 }
 
-//work on the convertion to string 
+//work on the convertion to string
+/*
 string Fraction :: convert_fraction(){
      std::to_string(num);
      std::to_string(den);
@@ -55,11 +56,13 @@ string Fraction :: convert_fraction(){
     return " " + num + "/" + den;
     
 }
+*/
 
-void Fraction::Print_fraction(){
-    
-    outfile<<
-    
+ void Fraction::Print_fraction(){
+     
+     
+     
+  cout << " " <<getNumerator() << "/" << getDenominator();
 }
 
 
@@ -69,6 +72,106 @@ void Fraction::Print_fraction(){
 
 
 
+
+
+
+ void Fraction ::  setNumerator(int n){
+    
+    if( n < 0 ){
+        num = 0;
+    }
+    else{
+        num = n;
+    }
+}
+
+void Fraction :: setDenominator(int d){
+    if(d <= 0){
+        
+        den = 1;
+    }
+    else {
+        den = d;
+    }
+    
+}
+
+
+int Fraction :: getNumerator(){
+    
+    return num;
+}
+
+
+int Fraction :: getDenominator(){
+    
+    return den;
+}
+
+
+void Fraction :: convertion(){
+    
+    
+    
+}
+
+ostream& operator<<(ostream &out, Fraction f)
+{   //prints out a fraction which doesn't simplify to a mixed fraction or
+    //whole number.
+    if((f.num > 0) && (f.den > 1) && ((f.num / f. den) < 0)){
+    out << f.num << "/" << f.den;
+    
+  
+    }
+    //when the fraction has a nummerator that is higher than the denominator and the
+    //denominator equals zero. it prints out the whole number answer.
+    else if ( (f.num > f.den) && (f.den == 1)){
+        int wholenumber = f.num/f.den;
+        
+        out << wholenumber;
+        
+        
+    }
+    //prints out a whole number if the fraction num and den are equal to each other.
+    else if(f.num == f.den){
+        
+        int wholenumber = f.num / f.den;
+        
+        out << wholenumber;
+    }
+    
+    //print out a whole number if the fraction can divide evenly 6/3.
+    else if( (f.num % f.den == 0) &&(f.den < f.num)){
+        
+        int wholenumber = f.num / f.den;
+        
+        out << wholenumber;
+        
+    }
+    else if((f.num > 0) && (f.den > 1) && (f.num / f. den > 0)){
+        
+        int wholenumber = f.num / f.den;
+        int  new_num = f.num % f.den;
+        
+        out << wholenumber << " " << new_num << "/" << f.den;
+        
+        
+        
+    }
+    else if( f.num == 0){
+        
+        out << f.num;
+        
+    }
+    else
+        
+        
+        
+        
+        return out;
+    
+    
+}
 
 
 /*Fraction Fraction :: sum(Fraction frac1, Fraction frac2){
--- Prog4fractions/Prog4fractions/Fraction_Isaac.h
+++ Prog4fractions/Prog4fractions/Fraction_Isaac.h
@@ -8,12 +8,23 @@ private:
     int num,den;
     
 public:
+    friend std::ostream& operator<<(std::ostream &out, Fraction f);
     Fraction();
     Fraction(int n , int d);
     double toDecimal();
     void reduce_fraction();
     string convert_fraction();
+    void Print_fraction();
     
+    void  setNumerator(int n);
+    void  setDenominator(int d);
+    
+    int  getNumerator();
+    int  getDenominator();
+    
+   
+    
+    void convertion();
     
     
 };
